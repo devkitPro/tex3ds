aa92955bfd06f3a4298d469eb330ec06d6113655
Michael Theall <pigman46@gmail.com>
2022-08-04 23:37:03 -0500

    Fix memory leak

--- source/huff.cpp
+++ source/huff.cpp
@@ -536,7 +536,7 @@ std::vector<uint8_t> huffEncode (const void *source, size_t len)
 	}
 
 	// we're done with the Huffman tree and lookup table
-	root.release ();
+	root.reset ();
 	lookup.clear ();
 
 	// flush the bitstream
